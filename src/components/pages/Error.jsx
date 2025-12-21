f4de0eeb03e2b0e80fd8172d9af4f5cb270a6ff9
Sijan666 <666majharulislam@gmail.com>
2025-12-22 02:46:56 +0600

    Refactor code structure for improved readability and maintainability

--- src/components/pages/Error.jsx
+++ src/components/pages/Error.jsx
@@ -1,19 +1,20 @@
-// import Images from "../Images"
-// import Button from "../Button"
-// import { Link } from "react-router-dom"
+import Images from "../Images"
+import error2 from '../../assets/error2.gif'
+import Button from "../Button"
+import { Link } from "react-router-dom"
 
 
 const Error = () => {
     return (
         <>
-        {/* <div className="relative">
-            <Images imgSrc={} className={'mx-auto mt-[100px]'}/>
+        <div className="relative">
+            <Images imgSrc={error2} className={'mx-auto mt-[100px]'}/>
             <div className="errorBtn text-center absolute left-[50%] -translate-x-[50%] bottom-[70px] ">
                 <Link to={"/"}>
                 <Button btnText={"Back To Homepage"} className={"cursor-pointer text-[#39AD31]! font-semibold! bg-transparent hover:bg-[#39AD31]! border boder-1 border-[#39AD31]! hover:border-transparent hover:text-white! duration-300"}/>
                 </Link>
             </div>
-        </div> */}
+        </div>
         </>
     )
 }
